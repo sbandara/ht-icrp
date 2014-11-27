2fbae6d44203e4730a18bcce242b72ead0b4b3c2
Samuel Bandara <Samuel_Bandara@hms.harvard.edu>
2014-11-26 23:40:29 -0500

    Recursive implementation of segmorph.

--- segmorph.m
+++ segmorph.m
@@ -1,38 +1,26 @@
-function [tseg, best_m, cost] = segmorph(edge)
+function [tseg, least_cost] = segmorph(edge, n_bnd)
 % SEGMORPH  Segment death metric in the time domain.
 %   [TSEG, BEST_M, COST] = SEGMORPH(EDGE) segments trajectories of death
-%   metrices and finds the three most likely change points according to a
-%   least squares assumption. EDGE must be a vector of the death readout
-%   (tested for "area", "edge", and "momp"). TSEG is the vector of three
-%   changepoints, BEST_M is the best fit vector of means, and COST is the
-%   sum of residuals. Sam
-m = zeros(4, 1);
+%   metrics and finds the N_BND most likely change points according to a
+%   least mean residual assumption. EDGE must be a vector of the death
+%   readout (tested for "area", "edge", and "momp"). TSEG is the vector
+%   of three changepoints, and LEAST_COST is the sum of residuals. Sam
 n_t = length(edge);
+least_cost = inf;
 tseg = [];
-cost = inf;
-best_m = [];
-for idx1 = 2 : n_t - 1
-    seg_edge = edge(1 : idx1);
-    m(1) = mean(seg_edge);
-    res0 = sum((m(1) - seg_edge) .^ 2);
-    for idx2 = idx1 + 1 : n_t - 1
-        res1 = res0;
-        seg_edge = edge(idx1 + 1 : idx2);
-        m(2) = mean(seg_edge);
-        res1 = res1 + sum((m(2) - seg_edge) .^ 2);
-        for idx3 = idx2 + 1 : n_t - 1
-            res = res1;
-            seg_edge = edge(idx2 + 1 : idx3);
-            m(3) = mean(seg_edge);
-            res = res + sum((m(3) - seg_edge) .^ 2);
-            seg_edge = edge(idx3 + 1 : end);
-            m(4) = mean(seg_edge);
-            res = res + sum((m(4) - seg_edge) .^ 2);
-            if res < cost
-                cost = res;
-                tseg = [idx1, idx2, idx3];
-                best_m = m;
-            end
-        end
+tseg_sub = [];
+for idx = 1 : n_t - n_bnd
+    seg_edge = edge(1 : idx);
+    cost = sum(abs(mean(seg_edge) - seg_edge));
+    if n_bnd == 1
+        seg_edge = edge(idx + 1 : end);
+        cost = cost + sum(abs(mean(seg_edge) - seg_edge));
+    else
+        [tseg_sub, cost_sub] = segmorph(edge(idx + 1 : end), n_bnd - 1);
+        cost = cost + cost_sub;
+    end
+    if cost < least_cost
+        least_cost = cost;
+        tseg = [idx, tseg_sub + idx];
     end
 end
